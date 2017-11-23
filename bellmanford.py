693d0f4da9bd8f32581dcf03abc26755455a8122
Eryk <rcksorto@yahoo.com>
2017-11-22 23:15:28 -0800

    Load topology from file

--- bellmanford.py
+++ bellmanford.py
@@ -17,13 +17,26 @@ def bellman_ford(graph, source):
     return d, p
 
 
+def load_topology(graph, filename):
+    with open(filename, 'r') as topology:
+        lines = topology.readlines()
+        num_of_nodes = int(lines[0])
+
+        for node in range(num_of_nodes):
+            graph.setdefault(node + 1, {})
+
+        for line in lines[2 + num_of_nodes:]:
+            tokens = map(int, line.split())
+            graph[tokens[0]][tokens[1]] = tokens[2]
+
+    return graph
+
+
 def main():
-    graph = {
-        'a': {'b': 7, 'c': 4, 'd': 5},
-        'b': {'a': 7, 'c': 2},
-        'c': {'a': 4, 'b': 2, 'd': 1},
-        'd': {'a': 5, 'c': 1},
-    }
+    graph = load_topology({}, 'topology-a.txt')
+    graph = load_topology(graph, 'topology-b.txt')
+    graph = load_topology(graph, 'topology-c.txt')
+    graph = load_topology(graph, 'topology-d.txt')
 
     for source in graph:
         d, p = bellman_ford(graph, source)
